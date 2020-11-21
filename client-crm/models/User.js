168a970adcef65bd12472a7675ce86a5978c395d
boichik <71084886+boichik@users.noreply.github.com>
2020-11-21 16:18:42 +0300

    Обновлена модель пользователя

--- client-crm/models/User.js
+++ client-crm/models/User.js
@@ -2,14 +2,7 @@ const mongosee = require('mongoose');
 const Schema = mongosee.Schema;
 
 const userSchema = new Schema({
-    userInfo:{name:String,email:{unique:true,type:String}},
-        
-    //       name: String,
-    //       email:{
-    //         unique: true,
-    //         type: String
-    //       }
-    // },
+    userInfo:{name:String,email:{unique:true,type:String},api_key:String},
     password: String
 })
 
