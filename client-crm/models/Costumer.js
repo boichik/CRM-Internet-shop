521efd642511a7b99e1bd747b38927d659a0c019
boichik <71084886+boichik@users.noreply.github.com>
2020-11-21 16:15:30 +0300

    Добавлена модель покупателя

---
+++ client-crm/models/Costumer.js
@@ -0,0 +1,13 @@
+const mongosee = require('mongoose');
+const Schema = mongosee.Schema;
+
+const costumerSchema = new Schema({
+    user_id:String,
+    bio:String,
+    phone:Number,
+    email:String,
+    orders:[{order_id:String, address:String, order_bio:String}]
+})
+
+const Costumer = mongosee.model('Costumer', costumerSchema);
+module.exports =Costumer;
